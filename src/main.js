1e77915e6c8bdc3b2ccfec64555438b5fedbb7dd
Hyeonseok Kang <kekemusa37@gmail.com>
2025-08-26 21:41:57 +0900

    [클린코드 8기 강현석] 자동차 경주 미션 STEP 1 (#334)
    * feat: 자동차는 이름을 상태로 가질 수 있습니다
    
    * feat: 자동차는 위치 값을 가지며, 초기 상태는 0이다
    
    * feat: 자동차는 전진할 수 있으며 한 번에 1만큼 전진한다
    
    * chore: indent 규칙을 적용합니다
    
    * chore: 세미콜론을 찍습니다
    
    * feat: foward 함수의 파라미터로 값을 전달받을 수 있도록 합니다

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log("Car");
+class Car {
+  static FORWARD_STEP = 1;
+
+  constructor(name, position = 0) {
+    this.name = name;
+    this.position = position;
+  }
+
+  forward(position = FORWARD_STEP) {
+    this.position += position;
+  }
+}
