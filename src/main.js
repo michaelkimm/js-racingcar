7eeeca5fed49d46339c42d315005ceebbd78950f
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-08-26 22:41:56 +0900

    feat: 첫 번째 요구사항 구현 (#328)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log("Car");
+class Car { 
+    constructor(name) {
+        this.name = name;
+        this.location = 0;
+    }
+
+    move() {
+        this.location += this.location;
+    }
+}
