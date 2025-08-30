dd4bf16708a0ee49fe0d180dccb57d65b2057632
michaelkimm <alstjrdl951@gmail.com>
2025-08-30 19:50:01 +0900

    ref:describe의 이름을 명사로 변경

--- __tests__/car.test.js
+++ __tests__/car.test.js
@@ -1,6 +1,6 @@
 import {Car} from '../src/car';
 
-describe('자동차 클래스 테스트', () => {
+describe('자동차', () => {
 
   test('이름을 상태로 가진다', () => {
     // given
@@ -13,7 +13,7 @@ describe('자동차 클래스 테스트', () => {
     expect(car.name).toEqual(carName);
   })
 
-  test('자동차는 위치 값을 가지며 초기 상태는 0이다.', () => {
+  test('위치 값을 가지며 초기 상태는 0이다.', () => {
     // when
     let car = new Car('anyName');
 
@@ -21,7 +21,7 @@ describe('자동차 클래스 테스트', () => {
     expect(car.position).toEqual(0);
   })
 
-  test('자동차는 전진할 수 있으며 한 번에 1만큼 전진한다.', () => {
+  test('전진할 수 있으며 한 번에 1만큼 전진한다.', () => {
     // given
     let car = new Car('anyName');
     expect(car.position).toEqual(0);
