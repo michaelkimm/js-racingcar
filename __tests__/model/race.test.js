9e8284c8184e9f8a15b7990f29d98e810b8d9e1a
michaelkimm <alstjrdl951@gmail.com>
2025-09-01 08:41:32 +0900

    Add enter line

--- __tests__/model/race.test.js
+++ __tests__/model/race.test.js
@@ -3,7 +3,7 @@ import { Race } from '../../src/model/race';
 
 describe('경주', () => {
 
-  test('는 5회 고정하여 실행한다.', () => {
+  test('는 5회 고정하여 실행하고, 자동차는 1회당 1칸씩 전진한다.', () => {
     // given
     const car = new Car('soo');
     const race = new Race(car);
