ec1cbc29863614a712458ec57ee5980feec6404b
michaelkimm <alstjrdl951@gmail.com>
2025-09-02 08:43:40 +0900

    ref:let대신 const 사용

--- __tests__/model/car.test.js
+++ __tests__/model/car.test.js
@@ -1,4 +1,4 @@
-import {Car} from '../../src/model/car';
+import { Car } from '../../src/model/car';
 
 describe('자동차', () => {
 
@@ -6,32 +6,32 @@ describe('자동차', () => {
 
     test('생성자로 설정할 수 있다.', () => {
       // given
-      let carName = "둥둥이";
-  
+      const carName = "둥둥이";
+
       // when
-      let car = new Car(carName);
-  
+      const car = new Car(carName);
+
       // then
       expect(car.name).toEqual(carName);
     })
 
     test.each([
-      ["", false], 
+      ["", false],
       ["12345", true],
       ["123456", false]
     ])('길이는 1이상 5이하여야 한다.', (name, expected) => {
       // when, then
       if (expected) {
-        expect(() => {new Car(name)}).not.toThrow();
+        expect(() => { new Car(name) }).not.toThrow();
       } else {
-        expect(() => {new Car(name)}).toThrow(new Error('자동차 이름은 1글자 이상, 5글자 이하여야 합니다.'));
+        expect(() => { new Car(name) }).toThrow(new Error('자동차 이름은 1글자 이상, 5글자 이하여야 합니다.'));
       }
     })
   })
 
   test('위치 값을 가지며 초기 상태는 0이다.', () => {
     // when
-    let car = new Car('name');
+    const car = new Car('name');
 
     // then
     expect(car.position).toEqual(0);
@@ -39,7 +39,7 @@ describe('자동차', () => {
 
   test('전진할 수 있으며 한 번에 1만큼 전진한다.', () => {
     // given
-    let car = new Car('name');
+    const car = new Car('name');
     const beforePosition = car.position;
 
     // when
